12abd0ac6f51c84874342100e297443220fb418b
CezinhaDev <cesarhenriquee04@gmail.com>
2025-09-30 09:36:33 -0300

    feat: inicializa projeto Spring Boot com README, estrutura e dependências

---
+++ src/test/java/ApiToDoList/ToDoList/ToDoListApplicationTests.java
@@ -0,0 +1,13 @@
+package ApiToDoList.ToDoList;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToDoListApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
